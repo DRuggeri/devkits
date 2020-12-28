316f4ddf92421e406a165d65361c96deba879e66
Daniel Ruggeri <druggeri@primary.net>
2020-12-27 19:52:52 -0600

    Move down to node 14 so sass will work, add webpack

--- node/runit.sh
+++ node/runit.sh
@@ -1,5 +1,5 @@
 if docker ps -f name=ndk -q | grep '^';then
   docker exec -it ndk bash
 else
-  docker run -it --name ndk --rm -v "$HOME/workspace":/root/workspace druggeri/ndk
+  docker run -it --name ndk --rm -v "$HOME/workspace":/root/workspace -p9000:9000 druggeri/ndk
 fi
